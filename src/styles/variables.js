c638c809e101b9c3fbc9f87c67c58921115163ad
Sebastià Limones Martí <lemon@Sebastias-MBP.home>
2020-07-01 16:31:59 +0200

    variables

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,11 +1,4 @@
 export const darkGray = '#1F1F1F';
 export const mediumGray = '#524F53';
 export const lightGray = '#D8D8D8';
-export const whiteHover = '#F8F8F8';
 export const primaryColor = '#EA777C';
-
-export const statusColorMapper = {
-  confirmed: 'red',
-  symptomatic: 'orange',
-  closed: 'green',
-};
