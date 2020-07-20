28ca9a0eafed2d423489bb0ad56b100acf16389a
Sebastià Limones Martí <lemon@Sebastias-MBP.home>
2020-07-20 11:11:50 +0200

    devAdminConfig

--- src/services/admin.js
+++ src/services/admin.js
@@ -3,28 +3,28 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/functions';
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-//   appID: process.env.REACT_APP_FIREBASE_APP_ID,
-// };
-
-const firebaseConfigDev = {
-  apiKey: process.env.REACT_APP_DEV_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_DEV_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DEV_FIREBASE_DB_URL,
-  projectId: process.env.REACT_APP_DEV_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_DEV_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_DEV_FIREBASE_SENDER_ID,
-  appID: process.env.REACT_APP_DEV_FIREBASE_APP_ID,
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appID: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// firebase.initializeApp(firebaseConfig);
-firebase.initializeApp(firebaseConfigDev);
+// const firebaseConfigDev = {
+//   apiKey: process.env.REACT_APP_DEV_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_DEV_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DEV_FIREBASE_DB_URL,
+//   projectId: process.env.REACT_APP_DEV_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_DEV_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_DEV_FIREBASE_SENDER_ID,
+//   appID: process.env.REACT_APP_DEV_FIREBASE_APP_ID,
+// };
+
+firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfigDev);
 
 export const auth = firebase.auth();
 export const db = firebase.firestore();
