2886b724fe2c047e82258e0d3f7f4ba122b55baa
sebastialimones <sebas.limones@gmail.com>
2023-07-19 12:36:44 +0200

    add handicap real

--- src/components/ListOfResults/index.js
+++ src/components/ListOfResults/index.js
@@ -34,6 +34,42 @@ export const ListOfResults = ({ results, playerHandicap, refreshResults }) => {
   const averageRatingCalculator = (ratings) => {
     return ratings.reduce((a,b) => (a + b)) / rating.length; 
   };
+
+  const realHandicapCalculator = () => {
+    const resultsWithCalculatedStrokes = [];
+    let averageHandicapResult = 0;
+
+    completeResult.map((game) => {
+      const handicapResult = [];
+      const mosca = 2.5;
+      game.completeResult.map((holeResult) => {
+        if(holeResult.strokes === 'mosca'){
+          handicapResult.push(mosca);
+        } else( 
+          handicapResult.push(holeResult.strokes - holeResult.par)
+          );
+        return undefined;
+      })
+      //calculte the total number of strokes
+      let totalStrokesPerGame = 0;
+      totalStrokesPerGame = handicapResult.reduce((a,b) => a + b, 0);
+      
+      //calculate the handicap according to the number of hole and add them to an array
+      let stokesAccordingToHolesPlayed = 0;
+      const totalHolesPerGame = 18;
+      if(game.completeResult.length < 18){
+        stokesAccordingToHolesPlayed = ( totalHolesPerGame * totalStrokesPerGame ) / game.completeResult.length; 
+        resultsWithCalculatedStrokes.push(stokesAccordingToHolesPlayed);
+      }else{
+        resultsWithCalculatedStrokes.push(totalStrokesPerGame)
+      };
+      return undefined;
+    });
+
+    //calculate the handicap
+    averageHandicapResult = resultsWithCalculatedStrokes.reduce((a,b) => a + b, 0) / (resultsWithCalculatedStrokes.length);
+    return averageHandicapResult;
+  };
   
   return(
     <Container>
@@ -46,6 +82,8 @@ export const ListOfResults = ({ results, playerHandicap, refreshResults }) => {
           ? `Rating medio: ${ averageRatingCalculator(rating).toFixed(2) }`
           : undefined 
           }
+          <br></br>
+          { `Handicap real: ${ realHandicapCalculator().toFixed(2) } golpes` }
         </TotalGames>
         : undefined
         }
